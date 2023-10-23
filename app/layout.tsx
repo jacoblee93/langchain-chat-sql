ade0f6f97f7c8ee969ffca15e52d1e0c80f64f9d
jacoblee93 <jacoblee93@gmail.com>
2023-10-22 18:18:47 -0700

    Update meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,23 +11,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>LangChain + Next.js Template</title>
+        <title>Talk to SQL With LangChain.js</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <meta
           name="description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+          content="Template showing how to query your SQL database with natural language. See source code and deploy your own at https://github.com/jacoblee93/langchain-chat-sql!"
         />
-        <meta property="og:title" content="LangChain + Next.js Template" />
+        <meta property="og:title" content="Talk to SQL With LangChain.js" />
         <meta
           property="og:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+          content="Template showing how to query your SQL database with natural language. See source code and deploy your own at https://github.com/jacoblee93/langchain-chat-sql!"
         />
         <meta property="og:image" content="/images/og-image.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LangChain + Next.js Template" />
+        <meta name="twitter:title" content="Talk to SQL With LangChain.js" />
         <meta
           name="twitter:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
+          content="Template showing how to query your SQL database with natural language. See source code and deploy your own at https://github.com/jacoblee93/langchain-chat-sql!"
         />
         <meta name="twitter:image" content="/images/og-image.png" />
       </head>
